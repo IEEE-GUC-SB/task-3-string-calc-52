c8ff98cf5371da152d3140200b28e852f371f3ae
Ibrahim <ibrahimaboras2@gmail.com>
2022-12-09 00:45:24 +0200

    ✅ made checkIfNewLinesAllowedToGetAdded

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@
 public class Calculator {
 
     public int Add(String numbers){
-        String[] array = numbers.split(",");
+        String[] array = numbers.split("[,\n]");
         int ans = 0;
         for(int i = 0; i<array.length; i++){
             ans += array[i].equals("") ? 0 : Integer.parseInt(array[i]);
