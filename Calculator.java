6672271fc689717f0a038c9c9b65ee03b05266d4
Ibrahim <ibrahimaboras2@gmail.com>
2022-12-09 00:31:06 +0200

    ✅ made checkAddingNumbersWorks pass

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,11 @@
 public class Calculator {
 
     public int Add(String numbers){
-        return 0;
+        String[] array = numbers.split(",");
+        int ans = 0;
+        for(int i = 0; i<array.length; i++){
+            ans += Integer.parseInt(array[i]);
+        }
+        return ans;
     }
 }
