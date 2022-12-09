a7a9e24113085c04842a45d609a0c6bdcc060f97
Ibrahim <ibrahimaboras2@gmail.com>
2022-12-09 02:23:33 +0200

    ✅ made supportDifferentDelimiterIfUserWants pass

--- Calculator.java
+++ Calculator.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 public class Calculator {
 
     public int Add(String numbers){
-        String nubers = numbers;
         String[] array;
         if(numbers.substring(0, 2).equals("//")){
             numbers = numbers.replace("//", "");
@@ -14,8 +13,6 @@ public class Calculator {
             array = numbers.split(temp);
         }
         else  array = numbers.split("[,\n]");
-        String y = "[," + numbers.substring(0) + "\n]";
-        
         int ans = 0;
         for(int i = 0;i<array.length; i++){
             ans += array[i].equals("") ? 0 : Integer.parseInt(array[i]);
